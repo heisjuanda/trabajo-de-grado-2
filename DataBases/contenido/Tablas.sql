98f1d858bfce10218dbe6adce7850700d75268a7
azcaratejuan <azcarate.juan@correounivalle.edu.co>
2024-09-18 13:56:58 -0500

    pre.pruebas

--- DataBases/contenido/Tablas.sql
+++ DataBases/contenido/Tablas.sql
@@ -74,3 +74,16 @@ ALTER SEQUENCE public.user_id_seq OWNED BY public."user".id;
 ALTER TABLE ONLY public."user" ALTER COLUMN id SET DEFAULT nextval('public.user_id_seq'::regclass);
 
 
+CREATE SEQUENCE public.answer_id_seq
+    AS INTEGER
+    START WITH 1
+    INCREMENT BY 1
+    NO MINVALUE
+    NO MAXVALUE
+    CACHE 1;
+
+
+ALTER SEQUENCE public.answer_id_seq OWNED BY public."answer".id;
+
+
+ALTER TABLE ONLY public."answer" ALTER COLUMN id SET DEFAULT nextval('public.answer_id_seq'::regclass);
