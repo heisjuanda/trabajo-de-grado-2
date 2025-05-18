a372335e8a1bbb7d07cbebaf29a8724a6c7ce9b8
Juan David Moreno A <jdmoreno@truora.com>
2025-05-18 12:32:13 -0500

    revert youtube videos intro course

--- Front/soft-skills-front/src/components/course/Course.js
+++ Front/soft-skills-front/src/components/course/Course.js
@@ -106,10 +106,10 @@ export default function Course() {
 
   const getCourseVideo = (courseId) => {
     const courseVideos = {
-      "253": "https://www.youtube.com/watch?v=i-htv81L04g",
-      "270": "https://www.youtube.com/watch?v=Xp45ayoX_4s&t=126s",
+      "253": "https://www.youtube.com/embed/cJUXxjOeoCk",
+      "270": "https://www.youtube.com/embed/pACWFYE1AUA",
     };
-    return courseVideos[courseId] || "https://www.youtube.com/watch?v=i-htv81L04g";
+    return courseVideos[courseId] || "https://www.youtube.com/embed/cJUXxjOeoCk";
   };
 
   useEffect(() => {
